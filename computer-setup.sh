9eb919ff33cb71ca027a986612820ef102a25611
Jay Morgan <jaypmorgan@users.noreply.github.com>
2020-01-16 16:02:45 +0000

    added flag for use on 3rd party servers

--- computer-setup.sh
+++ computer-setup.sh
@@ -1,35 +1,51 @@
 #!/bin/bash
 
-sudo apt update;
-sudo apt upgrade -y;
-sudo apt install vim tmux git;
-mkdir -p ~/workspace;
+install_software () {
+  echo 'Installing Software'
+  sudo apt update;
+  sudo apt upgrade -y;
+  sudo apt install vim tmux git;
+}
 
-# Register a new SSH key with github
-echo 'Enter your email address for github > '
-read GITHUB_EMAIL
+register_github() {
+  echo 'Registering Github Keys'
+  # Register a new SSH key with github
+  echo 'Enter your email address for github > '
+  read GITHUB_EMAIL
 
-echo 'Enter your name for github > '
-read GITHUB_USERNAME
+  echo 'Enter your name for github > '
+  read GITHUB_USERNAME
 
-echo "generating ssh for github (with account $GITHUB_EMAIL";
-ssh-keygen -t rsa -b 4096 -C "$GITHUB_EMAIL";
-eval "$(ssh-agent -s)" 2>&1 /dev/null;
-ssh-add ~/.ssh/id_rsa
+  echo "generating ssh for github (with account $GITHUB_EMAIL";
+  ssh-keygen -t rsa -b 4096 -C "$GITHUB_EMAIL";
+  eval "$(ssh-agent -s)" 2>&1 /dev/null;
+  ssh-add ~/.ssh/id_rsa
 
-git config --global user.name "$GITHUB_USERNAME"
-git config --global user.email "$GITHUB_EMAIL"
+  git config --global user.name "$GITHUB_USERNAME"
+  git config --global user.email "$GITHUB_EMAIL"
 
-cat ~/.ssh/id_rsa.pub
-echo "Copy and paste the public key into github then press enter";
-read IGNORE
+  cat ~/.ssh/id_rsa.pub
+  echo "Copy and paste the public key into github then press enter";
+  read IGNORE
+}
 
-# download dot files
-echo "Downloading dot files and updating user configuration";
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
-cd workspace && git clone git@github.com:jaypmorgan/dotfiles.git
-cp dotfiles/.vimrc ~/.;
-cp dotfiles/.tmux.conf ~/.;
-vim -c 'PluginInstall' -c 'qa!';
-source ~/.bashrc;
+install_dots () {
+  # download dot files
+  echo "Downloading dot files and updating user configuration";
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
+  
+  mkdir -p ~/workspace;
+  cd workspace && git clone git@github.com:jaypmorgan/dotfiles.git
+  cp dotfiles/.vimrc ~/.;
+  cp dotfiles/.tmux.conf ~/.;
+  vim -c 'PluginInstall' -c 'qa!';
+  source ~/.bashrc;
+}
 
+
+if ! [[ $* == *--server* ]]; then
+  install_software;
+fi
+
+register_github;
+install_dots;
